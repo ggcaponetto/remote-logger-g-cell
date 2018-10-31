507b444aa2c934811d07b3954dd9d597ad374d21
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2018-10-31 18:16:21 +0100

    add port parametrization

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![CircleCI](https://circleci.com/gh/ggcaponetto/remote-logger-g-cell.svg?style=svg)](https://circleci.com/gh/ggcaponetto/remote-logger-g-cell)
 
-# remote-logger-g-cell
+# remote-logger-g-cell (DO NOT USE IT NOW, IT'S IN DEVELOPMENT)
 
 This is a JavaScript remote logger. It enables you to read logs from a webapp running on any device such as smartphones
 and read the console logs in the browser, with minimal setup. Check out [vorlonjs.io](http://www.vorlonjs.io/)
@@ -15,3 +15,13 @@ Install the client library.
 In your webapp import the client library.
 
 ``import * as rlgc from "remote-logger-g-cell";``
+
+### start the server on a given ports
+
+``rlgc --mode server --ws-port 5000 --ui-port 3000``
+
+### start the console on a given port
+
+``rlgc --mode ui --ui-port 3333``
+
+Now you can open [http://localhost:3334/](http://localhost:3334/) and see your logs.
--- bin/global.js
+++ bin/global.js
@@ -3,28 +3,31 @@
 
 printArgs();
 var mode = getArgsValue("--mode");
-var port = getArgsValue("--port");
+var wsPort = getArgsValue("--ws-port");
+var uiPort = getArgsValue("--ui-port");
 
 
 var isWin = process.platform === "win32";
 console.log("you are runnung on windows: " + isWin);
-console.log("you are runnung with args: ", {mode: mode, port: port});
+console.log("you are runnung with args: ", {mode: mode, wsPort, uiPort});
 
-var spawn = require('child_process').spawn;
+function setEnvsAndRunScript(nodeArgs){
+    var spawn = require('child_process').spawn;
 
-var child = spawn(isWin ? "npm.cmd" : "npm", ["run", "rlgc-server"]);
+    var child = spawn(isWin ? "node.exe" : "node", nodeArgs);
 
-child.stdout.on('data', function (data) {
-    process.stdout.write(data);
-});
+    child.stdout.on('data', function (data) {
+        process.stdout.write(data);
+    });
 
-child.stderr.on('data', function (data) {
-    process.stdout.write(data);
-});
+    child.stderr.on('data', function (data) {
+        process.stdout.write(data);
+    });
 
-child.on('exit', function (data) {
-    process.stdout.write('done.');
-});
+    child.on('exit', function (data) {
+        process.stdout.write('done.');
+    });
+}
 
 function printArgs() {
     // print process.argv
@@ -37,4 +40,28 @@ function getArgsValue(arg) {
     if (process.argv.indexOf(arg) !== -1) {
         return process.argv[process.argv.indexOf(arg) + 1]
     }
+}
+
+switch (mode) {
+    case "server":
+        setEnvsAndRunScript(
+            [
+                "./node_modules/cross-env/dist/bin/cross-env-shell.js",
+                "RLGC_WS_PORT="+wsPort,
+                "RLGC_UI_PORT="+uiPort,
+                "npm run rlgc-server"
+            ]
+        );
+        break;
+    case "ui":
+        setEnvsAndRunScript(
+            [
+                "./node_modules/cross-env/dist/bin/cross-env-shell.js",
+                "PORT="+uiPort,
+                "npm run rlgc-ui"
+            ]
+        );
+        break;
+    default:
+        throw new Error("Invalid arguments calling 'rlgc'");
 }
